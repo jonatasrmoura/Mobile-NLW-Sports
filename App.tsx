c9da09eeb6a900315d60346916d373cb04a92db0
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2022-09-12 22:14:35 -0300

    feat: configurando ambiente

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello React-Native!</Text>
+      <Button title='Send 01' />
+      <Button title='Send 02' />
+      <Button title='Send 03' />
+      <Button title='Hello World' />
       <StatusBar style="auto" />
     </View>
   );
